e41cdf5fffff9ec61a5c3d2dafeff9220e3deb8e
jit <jit@DESKTOP-J1LGP8F>
2019-03-11 08:25:28 +0200

    Sample of GUI Programming...

--- src/GUI3/GUI.java
+++ src/GUI3/GUI.java
@@ -8,6 +8,7 @@ package GUI3;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -25,7 +26,7 @@ import javax.swing.JTextArea;
 public class GUI extends JFrame {
 
     public GUI() {
-        
+
         JLabel label1 = new JLabel("Printer : MyPrinter");
 
         JTextArea area1 = new JTextArea();
@@ -46,12 +47,16 @@ public class GUI extends JFrame {
         JScrollPane scrol2 = new JScrollPane(area2);
         scrol2.setPreferredSize(new Dimension(70, 70));
 
+        ButtonGroup group = new ButtonGroup();
         JPanel panel2 = new JPanel(new GridLayout(3, 1, 0, 0));
         JRadioButton Selection = new JRadioButton("Selection", false);
-        panel2.add(Selection);
         JRadioButton All = new JRadioButton("All", true);
-        panel2.add(All);
         JRadioButton Applet = new JRadioButton("Applet", false);
+        group.add(Selection);
+        group.add(All);
+        group.add(Applet);
+        panel2.add(Selection);
+        panel2.add(All);
         panel2.add(Applet);
 
         JTextArea area3 = new JTextArea();
@@ -85,9 +90,8 @@ public class GUI extends JFrame {
         panel5.add(label1);
         panel5.add(panel3);
         panel5.add(panel4);
-        
-        add(panel5);
 
+        add(panel5);
         JButton b1 = new JButton("OK");
         b1.setFocusPainted(false);
         JButton b2 = new JButton("Cancel");
@@ -102,7 +106,7 @@ public class GUI extends JFrame {
         panel6.add(b3);
         panel6.add(b4);
         add(panel6);
-        
+
         setTitle("Printer");
         setSize(500, 300);
         setLocationRelativeTo(null);
